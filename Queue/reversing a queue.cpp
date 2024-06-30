69506df0a9edc5e5af0c03a4f0369a17bc290541
Surendiran <137646072+surendiran04@users.noreply.github.com>
2024-06-30 20:24:10 +0530

    Update reversing a queue.cpp

--- Queue/reversing a queue.cpp
+++ Queue/reversing a queue.cpp
@@ -15,3 +15,39 @@ int main() {
     
     return 0;
 }
+
+//using stack and queue
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int n,k;
+    cin>>n>>k;
+    
+    queue<int> q;
+    for (int i = 0; i < n; ++i) {
+        int x;
+        cin >>x;
+        q.push(x);
+    }
+    
+    stack<int> s;
+    for(int i=0;i<k;i++){
+        s.push(q.front());
+        q.pop(); 
+    }
+    
+    while(!s.empty()){
+        q.push(s.top());
+        s.pop();
+    }
+    
+    for(int i=0;i<n-k;i++){
+        q.push(q.front());
+        q.pop();
+    }
+    
+   while(!q.empty()){
+       cout<<q.front()<<" ";
+       q.pop();
+   }
+    return 0;
+}
