c18d576a4f34d710eb875a919c0878e502052117
Surendiran <137646072+surendiran04@users.noreply.github.com>
2024-07-20 10:50:33 +0530

    Sorting | counting | hashmap counting

--- Math/LT-169.Majority element.cpp
+++ Math/LT-169.Majority element.cpp
@@ -28,3 +28,31 @@ public:
         return 0;
     }
 };
+
+class Solution { //best approach with linear time and O(1)space
+public:
+    int majorityElement(vector<int>& v) {
+        int cnt = 0;
+        int el;
+        int n = v.size();
+         for (int i = 0; i < n; i++) {
+            if (cnt == 0) {
+                cnt = 1;
+                el = v[i];
+            } else if (v[i] == el) {
+                cnt++;
+            } else {
+                cnt --;
+            }
+        }
+        int cnt1 = 0;
+        for (int i = 0; i < n; i++) {
+            if (v[i] == el)
+                cnt1++;
+        }
+        if (cnt1 > (n/ 2)) {
+            return el;
+        }
+        return -1;
+    }
+};
