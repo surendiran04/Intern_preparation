605faf4d4e6f5fa7c3089c24753fbcd3bf3b8e70
Surendiran <137646072+surendiran04@users.noreply.github.com>
2024-07-27 23:24:49 +0530

    Recursive approach and also iterative approach

---
+++ Stack/Sort stack.cpp
@@ -0,0 +1,42 @@
+//the top should contain the largest element and the last should contain smallest element
+// 5 -2 9 -7 3
+// Sample Output 
+// 9 5 3 -2 -7
+#include <bits/stdc++.h> 
+void insertSorted(stack<int> &s1,int x){ //T(n) =  O(n^2) ,S(n) =  O(n)
+	if(s1.empty() || x>s1.top()){
+		s1.push(x);
+	}
+	else{
+		int t=s1.top();
+		s1.pop();
+		insertSorted(s1,x);
+		s1.push(t);
+	}
+}
+void sortStack(stack<int> &s1)
+{
+	// Write your code here
+	if(!s1.empty()){
+		int x = s1.top();
+		s1.pop();
+		sortStack(s1);
+		insertSorted(s1,x);
+	}
+
+}
+//solved using loop
+while(!stk.isEmpty()){
+        int x=stk.pop();
+        if(stk1.isEmpty()){
+         stk1.push(x);
+        }
+    else{
+        while(x<=stk1.Top() && !stk1.isEmpty()){
+            stk2.push(stk1.pop());
+        }
+        stk1.push(x);
+        while(!stk2.isEmpty()){
+            stk1.push(stk2.pop());
+        }
+    }
