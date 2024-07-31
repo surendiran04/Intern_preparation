166a1ac611d44bfbbe8eb5a6fb0972606965cc73
Surendiran <137646072+surendiran04@users.noreply.github.com>
2024-07-31 06:21:07 +0530

    istringstream  and two-pointer

--- String/LT-151. Reverse Words in a String.cpp
+++ String/LT-151. Reverse Words in a String.cpp
@@ -20,3 +20,27 @@ public:
         return result;
     }
 };
+
+class Solution { //inplace swap
+public:
+    string reverseWords(string s) {
+        reverse(s.begin(),s.end()); 
+        int start=0,end=0,i=0;
+        int n=s.length();
+        while(i<n){
+            while(i<n && s[i]==' '){
+                i++;
+            }
+            if(i==n) break; //for stopping from unecessary spacing
+            while(i<n &&s[i]!=' '){
+                s[start++]=s[i++];
+            }
+            reverse(s.begin()+end,s.begin()+start); //reverse the word
+            s[start++]=' '; //manually adding space
+            end=start;      //end will take care of the new word's starting
+            i++;
+        }
+        s.resize(start-1);
+        return s;
+    }
+};
