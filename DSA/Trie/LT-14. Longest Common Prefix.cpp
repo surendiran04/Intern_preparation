5eb174f3edd2a96d4bf5374046ea846865ab2072
Surendiran <137646072+surendiran04@users.noreply.github.com>
2024-08-12 16:05:02 +0530

    flower flow flight - fl

---
+++ DSA/Trie/LT-14. Longest Common Prefix.cpp
@@ -0,0 +1,15 @@
+// Input: strs = ["flower","flow","flight"]
+// Output: "fl"
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if(strs.empty()) 
+            return "";
+         string prefix=strs[0];
+         for(string s:strs){
+            while(s.find(prefix)!=0)
+                prefix=prefix.substr(0,prefix.length()-1);
+         }
+         return prefix;
+    }
+};
