197cdfb7875bbac59d971207df3107ff3feed75e
Surendiran <137646072+surendiran04@users.noreply.github.com>
2024-08-15 09:11:59 +0530

    Reversal of 102.Level order traversal

---
+++ DSA/Tree/LT-107. Binary Tree Level Order Traversal II.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+         if (root == nullptr) {
+            return {};
+        }
+        vector<vector<int>> v;
+        queue<TreeNode*> q;
+        q.push(root);
+        while (!q.empty()) {
+            int m = q.size();
+            vector<int> ans;
+            for (int i = 0; i < m; i++) {
+                root = q.front();
+                q.pop();
+                ans.push_back(root->val);
+                if (root->left) {
+                    q.push(root->left);
+                }
+                if (root->right) {
+                    q.push(root->right);
+                }
+            }
+            v.push_back(ans);
+            ans.clear();
+        }
+        reverse(v.begin(),v.end());
+        return v;
+    }
+};
