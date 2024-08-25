3fd38355f5ffa0f9735da950c95228efedd75e7e
Surendiran <137646072+surendiran04@users.noreply.github.com>
2024-08-25 19:46:02 +0530

    Update LT-104. Maximum Depth of Binary Tree.cpp

--- DSA/Tree/LT-104. Maximum Depth of Binary Tree.cpp
+++ DSA/Tree/LT-104. Maximum Depth of Binary Tree.cpp
@@ -15,3 +15,31 @@ public:
        }
     }
 };
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+      if (root == nullptr) {
+            return 0;
+        }
+        queue<TreeNode*> q;
+        q.push(root);
+        int depth = 0;
+        int ans=0;
+        while (!q.empty()) {
+            int size = q.size();
+            for (int i = 0; i < size; ++i) {
+                TreeNode* node = q.front();
+                q.pop();
+                if (node->left) {
+                    q.push(node->left);
+                }
+                if (node->right) {
+                    q.push(node->right);
+                }
+            }
+            depth++;
+            ans=max(ans,depth);
+        }
+        return ans;
+    }
+};
