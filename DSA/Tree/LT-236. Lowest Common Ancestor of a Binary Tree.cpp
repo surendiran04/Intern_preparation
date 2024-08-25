4fc537e7fe8ee6ca2b7c9b2abfb912aa6f26041a
Surendiran <137646072+surendiran04@users.noreply.github.com>
2024-08-25 18:40:18 +0530

    Finiding the common path btwn 2 nodes | recursive traversal

--- DSA/Tree/LT-236. Lowest Common Ancestor of a Binary Tree.cpp
+++ DSA/Tree/LT-236. Lowest Common Ancestor of a Binary Tree.cpp
@@ -27,3 +27,21 @@ public:
         return false;
     }
 };
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) { //If l and r not nullptr means some node(root) has p,q as its child notes
+        if (root == nullptr || root == p || root == q) {
+            return root;
+        }
+        TreeNode* l =lowestCommonAncestor(root->left,p,q);
+        TreeNode* r =lowestCommonAncestor(root->right,p,q);
+        if(l==nullptr){
+            return r;
+        }
+        else if(r==nullptr){
+            return l;
+        }else{
+            return root;
+        }
+    }
+};
