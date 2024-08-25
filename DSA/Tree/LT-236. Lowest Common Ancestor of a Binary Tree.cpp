a2fb6cc829e7a954fa52dfbb35428440c43d87b1
Surendiran <137646072+surendiran04@users.noreply.github.com>
2024-08-25 18:32:38 +0530

    Generating all possible paths and then finding the common path

---
+++ DSA/Tree/LT-236. Lowest Common Ancestor of a Binary Tree.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (root == nullptr || root == p || root == q) {
+            return root;
+        }
+        vector<TreeNode*>pathP,pathQ;
+        getPath(root,pathP,p);
+        getPath(root,pathQ,q); //getting the path from the root to that node
+        TreeNode*ans;
+        for(int i=0;i<pathQ.size() && i<pathP.size();i++){ //the node which common between the node is the and among the equal nodes retuning the nearest one
+            if(pathQ[i]==pathP[i]){
+                ans=pathP[i];
+            }
+        }
+        return ans;
+    }
+    bool getPath(TreeNode* p, vector<TreeNode*>& v, TreeNode* x) {
+        if (!p)
+            return false;
+        v.push_back(p);
+        if (p == x)
+            return true;
+        if (getPath(p->left, v, x) || getPath(p->right, v, x))
+            return true;
+        v.pop_back();
+        return false;
+    }
+};
