2b34cc89612ab89c63344474a29e610f5c0ee17a
Surendiran <137646072+surendiran04@users.noreply.github.com>
2024-09-04 00:15:13 +0530

    Finding sorted whether left or right acc to mid

---
+++ DSA/BinarySearch/LT-33. Search in Rotated Sorted Array.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l = 0, h = nums.size() - 1;
+        while (l <= h) {
+            int m = (l + h) / 2;
+            if (nums[m] == target) {
+                return m;
+            } else if (nums[l] <= nums[m]) {
+                if (nums[l] <= target && target <= nums[m]) {
+                    h = m - 1;
+                } else {
+                    l = m + 1;
+                }
+            } else {
+                if (nums[h] >= target && target >= nums[m]) {
+                    l = m + 1;
+                } else {
+                    h = m - 1;
+                }
+            }
+        }
+        return -1;
+    }
+};
