22257a2ff29cd101e58c7584ea88da84bb1534ce
Surendiran <137646072+surendiran04@users.noreply.github.com>
2024-09-10 23:47:21 +0530

    bruteforce - copying arr twice then - monotonic stack approach

--- DSA/Stack/LT-503. Next Greater Element II.cpp
+++ DSA/Stack/LT-503. Next Greater Element II.cpp
@@ -25,3 +25,43 @@ public:
         return v;
     }
 };
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>arr(2*n);
+        for(int i=0;i<n;i++){
+            arr[i]=nums[i];
+            arr[i+n]=nums[i];
+        }
+        vector<int> ans(2*n,-1);
+        stack<int> st;
+        for(int i=0;i<2*n;i++){  //copying the array twice to make it circular
+            while(!st.empty() && arr[i]>arr[st.top()]){
+                    ans[st.top()]=arr[i];
+                    st.pop();
+            }
+            st.push(i);
+        }
+        copy(ans.begin(), ans.begin()+n, nums.begin());
+        return nums;
+    }
+};
+class Solution { //brute force
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>arr(n,-1);
+        for(int i=0;i<n;i++){
+            for(int j=1;j<n;j++){
+                int a=(i+j)%n;
+            if(nums[a]>nums[i]){
+                arr[i]=nums[a];
+                break;
+            }
+            }
+        }
+        
+        return arr;
+    }
+};
