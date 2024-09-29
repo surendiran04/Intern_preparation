1850ecd041018c4ab1baa0662294c7ae5ca370fe
Surendiran <137646072+surendiran04@users.noreply.github.com>
2024-09-29 19:03:40 +0530

    Two pointer swapping + visa interview qn

--- DSA/Array/LT-283. Move Zeroes.cpp
+++ DSA/Array/LT-283. Move Zeroes.cpp
@@ -18,3 +18,36 @@ public:
         }
     }
 };
+
+#include <iostream> //zeros inn the front
+#include <vector>
+using namespace std;  //question asked to me in the visa 2nd round interview
+
+class Solution {
+public:
+    void moveZeroesToFront(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 1) {
+            return;
+        }
+        int l = n - 1;
+        for (int r = n - 2; r >= 0; r--) {
+            if (nums[l] == 0 && nums[r] != 0) {
+                swap(nums[l], nums[r]);
+                l--;
+            } else if (nums[l] != 0) {
+                l--;
+            }
+        }
+    }
+};
+
+int main() {
+    vector<int> nums = {0, 3, 0, 1, 12};
+    Solution().moveZeroesToFront(nums);
+    for (int num : nums) {
+        cout << num << " ";
+    }
+    return 0;
+}
+
