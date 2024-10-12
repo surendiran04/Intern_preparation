f83c6a343836affcd2ec81fca187daed02deacf8
Surendiran <137646072+surendiran04@users.noreply.github.com>
2024-10-12 23:28:34 +0530

    Checking if there min ending overlaps with the arrival time means pop out

--- DSA/Heap/LT-2406. Divide Intervals Into Minimum Number of Groups.cpp
+++ DSA/Heap/LT-2406. Divide Intervals Into Minimum Number of Groups.cpp
@@ -1,3 +1,18 @@
+// Return the minimum number of groups you need to make.Two intervals intersect if there is at least one common number between them.
+// For example, the intervals [1, 5] and [5, 8] intersect.
+// Example 1:
+// Input: intervals = [[5,10],[6,8],[1,5],[2,3],[1,10]]
+// Output: 3
+// Explanation: We can divide the intervals into the following groups:
+// - Group 1: [1, 5], [6, 8].
+// - Group 2: [2, 3], [5, 10].
+// - Group 3: [1, 10].
+// It can be proven that it is not possible to divide the intervals into fewer than 3 groups.
+    
+// Example 2:
+// Input: intervals = [[1,3],[5,6],[8,10],[11,13]]
+// Output: 1
+    
 class Solution {
 public:
     int minGroups(vector<vector<int>>& intervals) {
