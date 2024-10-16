8f721d0601b7ce9889acb49eb55b458c57d737d4
Surendiran <137646072+surendiran04@users.noreply.github.com>
2024-10-16 13:30:02 +0530

    Visa interview qn | similar to 283.Move zeros

--- DSA/String/LT-2938. Separate Black and White Balls.cpp
+++ DSA/String/LT-2938. Separate Black and White Balls.cpp
@@ -12,7 +12,7 @@ public:
         long long cnt=0;
         for(int r=n-2;r>=0;r--){
             if(s[l]=='0' && s[r]=='1'){
-                swap(s[l],s[r]);
+                swap(s[l],s[r]); //transform the original array
                 cnt+=l-r;
                 l--;
             }else if(s[l]!='0'){
@@ -20,5 +20,19 @@ public:
             }
         }
         return cnt;
+    }
+};
+class Solution {
+public:
+    long long minimumSteps(string s) {
+        long long swap = 0;
+        int black = 0; 
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '0')  //for every zeros - we need to add the no of 1's in its left side
+                swap += (long long) black; 
+            else
+                black++; 
+        }
+        return swap;
     }
 };
