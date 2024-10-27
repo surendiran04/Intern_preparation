1af79302105cc917dba85900b4f8e3e5da21320a
Surendiran <137646072+surendiran04@users.noreply.github.com>
2024-10-28 00:09:10 +0530

    Trying out every possible combination | Slightly optimised

--- DSA/BackTracking/LT-1593. Split a String Into the Max Number of Unique Substrings.cpp
+++ DSA/BackTracking/LT-1593. Split a String Into the Max Number of Unique Substrings.cpp
@@ -3,7 +3,7 @@
 // Explanation: One way to split maximally is ['a', 'b', 'ab', 'c', 'cc']. 
 class Solution {
 public:
-    int maxUniqueSplit(string s) {
+    int maxUniqueSplit(string s) { //T(n) - exponential
         set<string> set;
         int maxSize=0;
         backtrack(s,0,set,maxSize);
@@ -20,5 +20,24 @@ public:
                 set.erase(c);
             }
         }
+    }
+};
+class Solution { //T(n) - O(2^n)
+public:
+    int maxUniqueSplit(string s) {
+        unordered_set<string> set;
+        return backtrack(s,0,set);
+    }
+    int backtrack(string& s,int start,unordered_set<string> &set){
+        int max_size=set.size();
+        for(int end=start;end<s.size();end++){
+            string c = s.substr(start,end-start+1);
+            if(set.find(c)==set.end()){
+                set.insert(c);
+                max_size=max(max_size,backtrack(s,end+1,set));
+                set.erase(c);
+            }
+        }
+        return max_size;
     }
 };
