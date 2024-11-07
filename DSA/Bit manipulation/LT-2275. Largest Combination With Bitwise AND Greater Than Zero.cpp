6aa20c7ace3eddb07d651bdebabfee59e135bba8
Surendiran <137646072+surendiran04@users.noreply.github.com>
2024-11-07 20:32:46 +0530

    & and right shift >>

--- DSA/Bit manipulation/LT-2275. Largest Combination With Bitwise AND Greater Than Zero.cpp
+++ DSA/Bit manipulation/LT-2275. Largest Combination With Bitwise AND Greater Than Zero.cpp
@@ -24,3 +24,20 @@ public:
         return largest;
     }
 };
+
+class Solution {
+public:
+    int largestCombination(vector<int>& candidates) {
+        vector<int> bitCount(24, 0);
+        for (int i = 0; i < 24; i++) {
+            for (int num : candidates) {
+                // Check if the i-th bit is set.
+                if ((num & (1 << i)) != 0) {
+                    bitCount[i]++;
+                }
+            }
+        }
+        // Return the maximum count.
+        return *max_element(bitCount.begin(), bitCount.end());
+    }
+};
