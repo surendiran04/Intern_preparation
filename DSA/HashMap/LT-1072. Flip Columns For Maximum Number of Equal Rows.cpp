1ea2021d0d136847a234ca480a93c236fce8cafa
Surendiran <137646072+surendiran04@users.noreply.github.com>
2024-11-27 23:45:48 +0530

    Here try to find the pattern like TTF - 001 and TTF 110 are same pattern

--- DSA/HashMap/LT-1072. Flip Columns For Maximum Number of Equal Rows.cpp
+++ DSA/HashMap/LT-1072. Flip Columns For Maximum Number of Equal Rows.cpp
@@ -1,26 +1,22 @@
+// Want to make the whole row the same by flipping the col of every cell
+// Return the maximum number of rows that have all values equal after some number of flips.
+// Input: matrix = [[0,0,0],[0,0,1],[1,1,0]]
+// Output: 2  If we flip the first 2 cols then matrix = [[1,1,0],[1,1,1],[0,0,0]] -last 2 rows are fully equal
 class Solution {
 public:
     int maxEqualRowsAfterFlips(vector<vector<int>>& matrix) {
-              unordered_map<string, int> patternFrequency;
-
+         unordered_map<string, int> patternFrequency;
         for (auto& currentRow : matrix) {
             string patternBuilder = "";
-
-            // Convert row to pattern relative to its first element
             for (int col = 0; col < currentRow.size(); col++) {
-                // 'T' if current element matches first element, 'F' otherwise
                 if (currentRow[0] == currentRow[col]) {
                     patternBuilder += "T";
                 } else {
                     patternBuilder += "F";
                 }
             }
-
-            // Convert pattern to string and update its frequency in map
             patternFrequency[patternBuilder]++;
         }
-
-        // Find the pattern with maximum frequency
         int maxFrequency = 0;
         for (auto& entry : patternFrequency) {
             maxFrequency = max(entry.second, maxFrequency);
