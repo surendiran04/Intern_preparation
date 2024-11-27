ac4490d06220c6e9be755485f6b1f1abdaad4f31
Surendiran <137646072+surendiran04@users.noreply.github.com>
2024-11-28 00:08:07 +0530

    Simple negative count, odd means subtracting from the existing sum

--- DSA/Matrix/LT-1975. Maximum Matrix Sum.cpp
+++ DSA/Matrix/LT-1975. Maximum Matrix Sum.cpp
@@ -1,23 +1,21 @@
+//We can multiply 2 adjacent elements by -1 if they are negative and make it positive and give the maximum possible sum
 class Solution {
 public:
     long long maxMatrixSum(vector<vector<int>>& matrix) {
         long long totalSum = 0;
         int minAbsVal = INT_MAX;
         int negativeCount = 0;
-
         for (auto& row : matrix) {
             for (int val : row) {
                 totalSum += abs(val);
                 if (val < 0) {
                     negativeCount++;
+                    minAbsVal = min(minAbsVal, abs(val));
                 }
-                minAbsVal = min(minAbsVal, abs(val));
             }
         }
-
-        // Adjust if the count of negative numbers is odd
-        if (negativeCount % 2 != 0) {
-            totalSum -= 2 * minAbsVal;
+        if (negativeCount % 2 != 0) { //If there are odd no of negative values then we can't make one element as minus
+            totalSum -= 2 * minAbsVal;  // so we minus it from the totalSum and the existing sum for it's minus sign
         }
 
         return totalSum;
