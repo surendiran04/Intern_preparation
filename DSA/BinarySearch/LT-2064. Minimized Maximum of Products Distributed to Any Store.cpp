ee71fdab13802e037b55309c606c5a5f050290ff
Surendiran <137646072+surendiran04@users.noreply.github.com>
2024-11-27 18:22:37 +0530

    Finding whether it is feasible by finding mid-point by binary search

--- DSA/BinarySearch/LT-2064. Minimized Maximum of Products Distributed to Any Store.cpp
+++ DSA/BinarySearch/LT-2064. Minimized Maximum of Products Distributed to Any Store.cpp
@@ -1,10 +1,20 @@
+// we hvae to distribute m diff types of products to n stores where we can distribute atmost one product to each store.
+// Return the minimum possible distribution.
+
+// Example 1:
+// Input: n = 6, quantities = [11,6]  
+// Output: 3   = max(1-3,2-3,3-3,4-3,5-3,6-2)
+    
+// Example 2:
+// Input: n = 7, quantities = [15,10,10]
+// Output: 5
+    
 class Solution {
 public:
     int minimizedMaximum(int n, vector<int>& quantities) {
         int low=1;
         int high=*max_element(quantities.begin(),quantities.end());
         int res=high;
-        float sum=0;
         while(low<=high){
             int mid = low + (high-low)/2;
             if(isFeasible(quantities,n,mid)){
@@ -19,7 +29,7 @@ public:
     bool isFeasible(vector<int>& products,int n,int maxShop){
         int req_shops=0;
         for(int p:products){
-            req_shops+=ceil((double)p/maxShop);
+            req_shops+=ceil((double)p/maxShop);  //distributing by finding a mid but of it is not optimal then chnage the mid
             if(req_shops>n){
                 return false;
             }
