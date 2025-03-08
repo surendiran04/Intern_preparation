f4636a9759604f19e8d95c92376e035bbb3568ac
Surendiran <137646072+surendiran04@users.noreply.github.com>
2025-03-08 22:21:21 +0530

    Window with 2 pointers - min whites

--- DSA/SlidingWindow/LT-2379. Minimum Recolors to Get K Consecutive Black Blocks.cpp
+++ DSA/SlidingWindow/LT-2379. Minimum Recolors to Get K Consecutive Black Blocks.cpp
@@ -29,3 +29,25 @@ public:
         return minlen;
     }
 };
+
+class Solution {
+public:
+    int minimumRecolors(string blocks, int k) {
+        int left = 0, numWhites = 0, numRecolors = INT_MAX;
+
+        for (int right = 0; right < blocks.size(); right++) {
+            if (blocks[right] == 'W') {
+                numWhites++;
+            }
+
+            if (right - left + 1 == k) {  //window size
+                numRecolors = min(numRecolors, numWhites);
+
+                if (blocks[left] == 'W') numWhites--;  //once the window moves
+
+                left++;
+            }
+        }
+        return numRecolors;
+    }
+};
