ce9a16e39e4a11656417bb4162825da63f27bd34
Surendiran <137646072+surendiran04@users.noreply.github.com>
2025-03-11 17:54:25 +0530

    Substring abc atleast once | count

--- DSA/SlidingWindow/LT-1358. Number of Substrings Containing All Three Characters.cpp
+++ DSA/SlidingWindow/LT-1358. Number of Substrings Containing All Three Characters.cpp
@@ -1,6 +1,17 @@
+// Return the number of substrings containing at least one occurrence of all these characters a, b and c.
+// Input: s = "abcabc"
+// Output: 10
+// Explanation: The substrings containing at least one occurrence of the characters a, b and c are "abc", "abca", "abcab", "abcabc", "bca", "bcab", "bcabc", 
+// "cab", "cabc" and "abc" (again). 
+// Input: s = "aaacb"
+// Output: 3
+// Explanation: The substrings containing at least one occurrence of the characters a, b and c are "aaacb", "aacb" and "acb". 
+// Input: s = "abc"
+// Output: 1
+    
 class Solution {
 public:
-    int numberOfSubstrings(string s) {
+    int numberOfSubstrings(string s) { //Time limit exceeded
     int cnt=0;
     int n = s.length();
     for(int i=0;i<n;i++){
