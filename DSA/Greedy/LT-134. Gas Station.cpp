8038400840a9719d54df1ad088051284644fec55
Surendiran M <137646072+surendiran04@users.noreply.github.com>
2025-08-18 23:11:23 +0530

    similiar to kadane algorithm

---
+++ DSA/Greedy/LT-134. Gas Station.cpp
@@ -0,0 +1,38 @@
+// Input: gas = [1,2,3,4,5], cost = [3,4,5,1,2]
+// Output: 3
+// Explanation:
+// Start at station 3 (index 3) and fill up with 4 unit of gas. Your tank = 0 + 4 = 4
+// Travel to station 4. Your tank = 4 - 1 + 5 = 8
+// Travel to station 0. Your tank = 8 - 2 + 1 = 7
+// Travel to station 1. Your tank = 7 - 3 + 2 = 6
+// Travel to station 2. Your tank = 6 - 4 + 3 = 5
+// Travel to station 3. The cost is 5. Your gas is just enough to travel back to station 3.
+// Therefore, return 3 as the starting index.
+// Example 2:
+
+// Input: gas = [2,3,4], cost = [3,4,3]
+// Output: -1
+
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int n=gas.size();
+        int gasSum=0,costSum=0;
+        for(int i=0;i<n;i++){
+            gasSum+=gas[i];
+            costSum+=cost[i];
+        }
+        if(costSum>gasSum){
+            return -1;
+        }
+        int start=0;
+        for(int i=0;i<n;i++){
+            int pre=gas[i]-cost[i]; //check if the car has fuel to reach next station
+            if(pre<0){
+                pre=0;
+                start=i+1;
+            }
+        }
+        return start;
+    }
+};
