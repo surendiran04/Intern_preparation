fcfd4185fe51263e8281b2b59c2bbfdfa2fa0546
Surendiran M <137646072+surendiran04@users.noreply.github.com>
2025-08-20 00:25:42 +0530

    sorted approach | prefix substr approach

--- DSA/Trie/LT-14. Longest Common Prefix.cpp
+++ DSA/Trie/LT-14. Longest Common Prefix.cpp
@@ -2,7 +2,7 @@
 // Output: "fl"
 class Solution {
 public:
-    string longestCommonPrefix(vector<string>& strs) {
+    string longestCommonPrefix(vector<string>& strs) { //O(n^2)
         if(strs.empty()) 
             return "";
          string prefix=strs[0];
@@ -11,5 +11,21 @@ public:
                 prefix=prefix.substr(0,prefix.length()-1);
          }
          return prefix;
+    }
+};
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& v) {
+        string ans="";
+        sort(v.begin(),v.end());
+        int n=v.size();
+        string first=v[0],last=v[n-1];
+        for(int i=0;i<min(first.size(),last.size());i++){
+            if(first[i]!=last[i]){
+                return ans;
+            }
+            ans+=first[i];
+        }
+        return ans;
     }
 };
