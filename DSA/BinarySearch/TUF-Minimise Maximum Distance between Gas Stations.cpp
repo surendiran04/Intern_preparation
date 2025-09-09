fa130e356071a2b0747c772fd85be7f64185ca68
Surendiran M <137646072+surendiran04@users.noreply.github.com>
2025-09-09 10:02:50 +0530

    long double | double precision needs to match 10^-1

---
+++ DSA/BinarySearch/TUF-Minimise Maximum Distance between Gas Stations.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long double minimiseMaxDistance(vector<int> &arr, int k) { TC: O(k*n) + O(n
+    int n = arr.size(); //size of array.
+    vector<int> howMany(n - 1, 0);
+
+    //Pick and place k gas stations:
+    for (int gasStations = 1; gasStations <= k; gasStations++) {
+        //Find the maximum section
+        //and insert the gas station:
+        long double maxSection = -1;
+        int maxInd = -1;
+        for (int i = 0; i < n - 1; i++) {
+            long double diff = arr[i + 1] - arr[i];
+            long double sectionLength =
+                diff / (long double)(howMany[i] + 1);
+            if (sectionLength > maxSection) {
+                maxSection = sectionLength;
+                maxInd = i;
+            }
+        }
+        //insert the current gas station:
+        howMany[maxInd]++;
+    }
+
+    //Find the maximum distance i.e. the answer:
+    long double maxAns = -1;
+    for (int i = 0; i < n - 1; i++) {
+        long double diff = arr[i + 1] - arr[i];
+        long double sectionLength =
+            diff / (long double)(howMany[i] + 1);
+        maxAns = max(maxAns, sectionLength);
+    }
+    return maxAns;
+}
+
+int main()
+{
+    vector<int> arr = {1, 2, 3, 4, 5};
+    int k = 4;
+    long double ans = minimiseMaxDistance(arr, k);
+    cout << "The answer is: " << ans << "\n";
+    return 0;
+}
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+long double minimiseMaxDistance(vector<int> &arr, int k) { //TC:O(nlogn + klogn)
+    int n = arr.size(); //size of array.
+    vector<int> howMany(n - 1, 0);
+    priority_queue<pair<long double, int>> pq;
+
+    //insert the first n-1 elements into pq
+    //with respective distance values:
+    for (int i = 0; i < n - 1; i++) {
+        pq.push({arr[i + 1] - arr[i], i});
+    }
+
+    //Pick and place k gas stations:
+    for (int gasStations = 1; gasStations <= k; gasStations++) {
+        //Find the maximum section
+        //and insert the gas station:
+        auto tp = pq.top();
+        pq.pop();
+        int secInd = tp.second;
+
+        //insert the current gas station:
+        howMany[secInd]++;
+
+        long double inidiff = arr[secInd + 1] - arr[secInd];
+        long double newSecLen = inidiff / (long double)(howMany[secInd] + 1);
+        pq.push({newSecLen, secInd});
+    }
+
+    return pq.top().first;
+}
+
+int main()
+{
+    vector<int> arr = {1, 2, 3, 4, 5};
+    int k = 4;
+    long double ans = minimiseMaxDistance(arr, k);
+    cout << "The answer is: " << ans << "\n";
+    return 0;
+}
