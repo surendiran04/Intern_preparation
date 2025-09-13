b58b3f3abc595633fc51217cabdb97350474c5c8
Surendiran M <137646072+surendiran04@users.noreply.github.com>
2025-09-13 13:24:56 +0530

    push to 2 init and pop out from 1 and put it in 1 and swap the name and use st1

--- DSA/Queue/queue using stack.cpp
+++ DSA/Queue/queue using stack.cpp
@@ -1,3 +1,56 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+class Stack {
+    queue<int> q1, q2;
+
+public:
+    void push(int x) {
+        q2.push(x);
+
+        // Move everything from q1 to q2
+        while (!q1.empty()) {
+            q2.push(q1.front());
+            q1.pop();
+        }
+
+        // Swap q1 and q2
+        swap(q1, q2);
+    }
+
+    void pop() {
+        if (q1.empty()) {
+            cout << "Stack is empty\n";
+            return;
+        }
+        q1.pop();
+    }
+
+    int top() {
+        if (q1.empty()) {
+            cout << "Stack is empty\n";
+            return -1;
+        }
+        return q1.front();
+    }
+
+    bool empty() {
+        return q1.empty();
+    }
+};
+
+int main() {
+    Stack st;
+    st.push(10);
+    st.push(20);
+    st.push(30);
+    cout << st.top() << endl; // 30
+    st.pop();
+    cout << st.top() << endl; // 20
+}
+
+
 //use the operations of stack and implement q
 sampleInput:
 // STDIN   Function
