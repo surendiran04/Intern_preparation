77ead6ecc4f604719f7a891c1930b7507e4702c7
Surendiran M <137646072+surendiran04@users.noreply.github.com>
2025-09-23 23:03:16 +0530

    Expand from i for odd and even sizes | brute force for all len and check

--- DSA/String/LT-5. Longest Palindromic Substring.cpp
+++ DSA/String/LT-5. Longest Palindromic Substring.cpp
@@ -7,7 +7,7 @@
 // Input: s = "cbbd"
 // Output: "bb"
  
-class Solution {
+class Solution {          
 public:
     string longestPalindrome(string s) { //O(n^3) didn't pass all TC's
         int n=s.length();
@@ -56,5 +56,39 @@ public:
             right++;
         }
         return s.substr(left+1,right-left-1);
+    }
+};
+
+class Solution {  //Top down DP
+public:
+    int dp[1001][1001];
+    string longestPalindrome(string s) {
+     int n=s.length();
+     memset(dp,-1,sizeof(dp));   // TC:O(n^2) SC:O(n^2)
+        int maxlen=INT_MIN,start=0;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                if(solve(s,i,j)){
+                    if(j-i+1>maxlen){
+                        maxlen=j-i+1;
+                        start=i;
+                    }
+                }
+            }
+        }
+        return s.substr(start,maxlen);
+    }
+    bool solve(string& s,int i,int j){
+        if(i>=j){
+            return true;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        if(s[i]==s[j]){
+            return dp[i][j]=solve(s,i+1,j-1);
+        }else{
+            return dp[i][j]=false;
+        }
     }
 };
