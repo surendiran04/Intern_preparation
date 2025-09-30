ecc13d0c136a470fdcdbaa2bb7bf7dc01c04cf9f
Surendiran M <137646072+surendiran04@users.noreply.github.com>
2025-09-30 23:20:37 +0530

    Trie approach | prefix | sort and first & last compare

--- DSA/Trie/LT-14. Longest Common Prefix.cpp
+++ DSA/Trie/LT-14. Longest Common Prefix.cpp
@@ -29,3 +29,83 @@ public:
         return ans;
     }
 };
+struct Node{
+    Node* links[26];
+    bool flag=false;
+    int childCount=0;
+    bool containsKey(char &ch){
+        return links[ch - 'a'] != NULL;
+    }
+
+    void put(char &ch , Node* node){
+        links[ch - 'a'] = node;
+    }
+
+    Node* get(char &ch){
+        return links[ch - 'a'];
+    }
+    void setEnd(){
+        flag=true;
+    }
+    bool isEnd(){
+        return flag;
+    }
+    void incChild(){
+        childCount++;
+    }
+    int getChildCount(){
+        return childCount;
+    }
+};
+class Trie{
+    private:
+    Node* root;
+    public:
+    Trie(){
+        root = new Node();
+    }
+
+    void insert(string word){
+        Node* node = root;
+        for(int i=0; i<word.size(); i++){
+            if(!node->containsKey(word[i]) ){
+                node->put(word[i] , new Node());
+            }
+            node = node->get(word[i]);
+            node->incChild();
+        }
+        node->setEnd();
+    }
+
+     string checkCompletePrefix(string word,int n){ 
+        Node* node = root;
+        string ans="";
+        for(int i=0; i<word.size(); i++){
+            if(node->containsKey(word[i])){
+                node = node->get(word[i]);
+                int x=node->getChildCount();
+                if(x==n){
+                    ans+=word[i];
+                }
+                if(node->isEnd() || x<n){
+                    return ans;
+                }  
+            }
+            else
+               return ans;
+        }
+
+        return ans;
+    }
+};
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& v) {
+    int n=v.size();
+    Trie t;
+    for(string& s:v){
+        t.insert(s);
+    }
+    return t.checkCompletePrefix(v[0],n);
+    }
+};
