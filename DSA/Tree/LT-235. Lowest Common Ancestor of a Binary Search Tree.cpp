99d670a7924263b72f1c1f44f3f05231fe6221b0
Surendiran M <137646072+surendiran04@users.noreply.github.com>
2025-10-01 12:02:23 +0530

    Update LT-235. Lowest Common Ancestor of a Binary Search Tree.cpp

--- DSA/Tree/LT-235. Lowest Common Ancestor of a Binary Search Tree.cpp
+++ DSA/Tree/LT-235. Lowest Common Ancestor of a Binary Search Tree.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution { //O(h) Time and O(h) Space
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) { //for binary search tree
         if(root->val > p->val && root->val > q->val){
@@ -11,6 +11,28 @@ public:
            return root;
     }
 };
+
+Node* LCA(Node* root, Node* n1, Node* n2) { //O(h) Time and O(1) Space
+
+    while (root != nullptr) {
+      
+        // If both n1 and n2 are smaller than root,
+        // then LCA lies in left
+        if (root->data > n1->data && root->data > n2->data)
+            root = root->left;
+
+        // If both n1 and n2 are greater than root,
+        // then LCA lies in right
+        else if (root->data < n1->data && root->data < n2->data)
+            root = root->right;
+        
+        // Else Ancestor is found
+        else
+            break;
+    }
+
+    return root;
+}
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
