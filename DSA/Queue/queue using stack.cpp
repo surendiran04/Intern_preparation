eff9e0df545be6dea75119c5912b0289982b13a1
Surendiran M <137646072+surendiran04@users.noreply.github.com>
2025-10-09 00:28:46 +0530

    Update queue using stack.cpp

--- DSA/Queue/queue using stack.cpp
+++ DSA/Queue/queue using stack.cpp
@@ -1,53 +1,50 @@
 #include <iostream>
-#include <queue>
+#include <stack>
 using namespace std;
 
-class Stack {
-    queue<int> q1, q2;
+class Queue {
+    stack<int> s1, s2;
 
 public:
-    void push(int x) {
-        q2.push(x);
-
-        // Move everything from q1 to q2
-        while (!q1.empty()) {
-            q2.push(q1.front());
-            q1.pop();
-        }
-
-        // Swap q1 and q2
-        swap(q1, q2);
+    void enqueue(int x) {
+        s1.push(x);
     }
 
-    void pop() {
-        if (q1.empty()) {
-            cout << "Stack is empty\n";
-            return;
-        }
-        q1.pop();
-    }
-
-    int top() {
-        if (q1.empty()) {
-            cout << "Stack is empty\n";
+    int dequeue() {
+        if (s1.empty() && s2.empty()) {
+            cout << "Queue is empty\n";
             return -1;
         }
-        return q1.front();
+
+        if (s2.empty()) {
+            // Transfer from s1 to s2
+            while (!s1.empty()) {
+                s2.push(s1.top());
+                s1.pop();
+            }
+        }
+
+        int topVal = s2.top();
+        s2.pop();
+        return topVal;
     }
 
     bool empty() {
-        return q1.empty();
+        return s1.empty() && s2.empty();
     }
 };
 
 int main() {
-    Stack st;
-    st.push(10);
-    st.push(20);
-    st.push(30);
-    cout << st.top() << endl; // 30
-    st.pop();
-    cout << st.top() << endl; // 20
+    Queue q;
+    q.enqueue(1);
+    q.enqueue(2);
+    q.enqueue(3);
+
+    cout << q.dequeue() << endl; // 1
+    cout << q.dequeue() << endl; // 2
+    q.enqueue(4);
+    cout << q.dequeue() << endl; // 3
+    cout << q.dequeue() << endl; // 4
 }
 
 
