cec1883c4603f1a942456e7cd81996abcbb3a055
Surendiran M <137646072+surendiran04@users.noreply.github.com>
2025-11-05 15:18:41 +0530

    BFS | 0-base indexing id setting | last-first+1

--- DSA/Tree/LT-662. Maximum Width of Binary Tree.cpp
+++ DSA/Tree/LT-662. Maximum Width of Binary Tree.cpp
@@ -3,28 +3,28 @@ Output: 4
 //The width of one level is defined as the length between the end-nodes (the leftmost and rightmost non-null nodes
 class Solution {
 public:
-    int widthOfBinaryTree(TreeNode* p) {
+    int widthOfBinaryTree(TreeNode* p) {  //Width - no of nodes could have there between any nodes of the same level
         queue<pair<TreeNode*,int>>q;
         int ans=0;
         q.push({p,0});
         while(!q.empty()){
             int n=q.size();
-            int m=q.front().second;
+            int mini=q.front().second;
             int first,last;
             for(int i=0;i<n;i++){
-                long c=q.front().second-m;  //making the every level's first node as 0 and last node no of nodes
+                long c=q.front().second-mini;
                 TreeNode* node=q.front().first;
                 q.pop();
                 if(i==0) first=c;
                 if(i==n-1) last=c;
                 if(node->left){
-                    q.push({node->left,c*2+1});
+                    q.push({node->left,c*2+1}); //O-Base Indexing
                 }
                  if(node->right){
                     q.push({node->right,c*2+2});
                 }
             }
-            ans=max(ans,last-first+1); //here we don't first since it is always 0 
+            ans=max(ans,last-first+1);
         }
         return ans;
     }
