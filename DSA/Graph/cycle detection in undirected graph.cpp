47800ab36e5edb42b80a1694b5fdc11d7e43b8fb
Surendiran M <137646072+surendiran04@users.noreply.github.com>
2025-11-19 18:16:29 +0530

    BFS | DFS - Parent & node | DSU

--- DSA/Graph/cycle detection in undirected graph.cpp
+++ DSA/Graph/cycle detection in undirected graph.cpp
@@ -53,3 +53,62 @@ for (auto &e : edges) {
         return false;
     }
 };
+
+
+class DSU{
+private:
+    vector<int> parent, union_size;
+
+public:    
+    DSU(int totalNodes){
+        parent.resize(totalNodes, -1);
+        // initially all are parent of iteslf and each set is having size 1
+        for(int i=0; i<totalNodes; i++) parent[i] = i;
+        union_size.resize(totalNodes, 1);
+        
+    }
+    int findRepresentative(int node){
+        if(parent[node]==node){
+            return node;
+        }
+        // find parent and store it so no need to calculate it again and again(path compression) like we do in DP.
+        return parent[node] = findRepresentative(parent[node]);
+        
+    }
+    
+    void doUnionBySize(int node1, int node2){
+        int parent1 = findRepresentative(node1);
+        int parent2 = findRepresentative(node2);
+        
+        // Both Node belong to same set
+        if(parent1==parent2) return;
+        
+        // always merge smaller set to larger set to avoid longer path
+        if(union_size[parent1] < union_size[parent2]){
+            parent[parent1] = parent2;
+            union_size[parent2] += union_size[parent1];
+        }else{
+            parent[parent2] = parent1;
+            union_size[parent1] += union_size[parent2];
+        }
+    }
+    
+};
+
+class Solution {
+  public:
+    bool isCycle(int V, vector<vector<int>>& edges) {
+        DSU dsu(V);
+        
+        for(auto &i: edges){
+            int parentU = dsu.findRepresentative(i[0]);
+            int parentV = dsu.findRepresentative(i[1]);
+            
+            if(parentU == parentV) return true;
+            dsu.doUnionBySize(i[0], i[1]);
+        }
+        
+        return false;
+        
+    }
+};
